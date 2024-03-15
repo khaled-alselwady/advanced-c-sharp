36c7df1a115445c4c911f54821d4aa5c44d63b67
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-15 03:00:22 +0300

    ADD Extension_Methods

---
+++ Extension Methods/Extension Methods Example/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extension_Methods_Example
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsWeekend(this DateTime value)
+        {
+            return value.DayOfWeek == DayOfWeek.Friday || value.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        public static bool IsWeekDay(this DateTime value)
+        {
+            return !IsWeekend(value);
+        }
+    }
+}
---
+++ Extension Methods/Extension Methods Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Methods_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine($"DateTime.Now: {date}");
+            Console.WriteLine($"DateTime.Now: {date.IsWeekend()}");
+            date = date.AddDays(3);
+            Console.WriteLine($"DateTime.Now: {date.IsWeekDay()}");
+
+            Console.ReadKey();
+        }
+    }
+}
