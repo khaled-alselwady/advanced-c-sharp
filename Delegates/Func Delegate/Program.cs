d305607fd0c64f3653b7176fa8da8460bc146937
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-18 00:56:41 +0300

    ADD Func_Delegate Example

---
+++ Delegates/Func Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+public class Program
+{
+    static Func<int, int> _square;
+
+    static int SquareMethod(int x) => x * x;
+
+    static void Main(string[] args)
+    {
+        _square = null;
+
+        int? result = _square?.Invoke(5);
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
