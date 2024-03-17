49d3beb781320da6bba56ae277b0991805a8b29f
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-18 01:09:15 +0300

    ADD Action_Delegate Example

---
+++ Delegates/Action Delegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+public class Program
+{
+    static void ActionWithParameterlessMethod()
+    {
+        Console.WriteLine("This is a parameterless method");
+    }
+
+    static void ActionWithIntParameter(int x)
+    {
+        Console.WriteLine($"Action with int parameter: {x}");
+    }
+
+    static void ActionWithMultipleParameters(string str, int x)
+    {
+        Console.WriteLine($"Action with Multiple parameters: {str},{x}");
+    }
+
+    static void Main(string[] args)
+    {
+        Action actionWithParameterlessMethod = ActionWithParameterlessMethod;
+        Action<int> actionWithIntParameter = ActionWithIntParameter;
+        Action<string, int> actionWithMultipleParameters = ActionWithMultipleParameters;
+
+        actionWithParameterlessMethod?.Invoke();
+        actionWithIntParameter?.Invoke(5);
+        actionWithMultipleParameters?.Invoke("Hello, World!", 5);
+
+        Console.ReadKey();
+    }
+}
