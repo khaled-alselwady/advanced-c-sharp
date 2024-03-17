68869f7ba3f06b7058c0acbe05d3f5c55b525338
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-18 01:18:10 +0300

    ADD Predicate_Delegate Example

---
+++ Delegates/Predicate Delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+public class Program
+{
+    static Predicate<int> _IsEvenPredicate = IsEven;
+
+    static bool IsEven(int x) => (x % 2 == 0);
+
+    static void Main(string[] args)
+    {
+        bool? result = _IsEvenPredicate?.Invoke(4);
+
+        Console.WriteLine(result);
+
+        Console.ReadKey();
+    }
+}
+
