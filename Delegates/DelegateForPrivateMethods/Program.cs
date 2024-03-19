2f4aade757ee8f63697d1efd280efa0feab33ce2
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-03-19 06:55:09 +0300

    ADD Delegate_For_Private_Methods Example

---
+++ Delegates/DelegateForPrivateMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public delegate int AddDelegate(int a, int b);
+
+public class Calculator
+{
+    private int _Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public AddDelegate GetAddDelegate()
+    {
+        return _Add;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        AddDelegate addDelegate = calc.GetAddDelegate();
+
+        int result = addDelegate(5, 3);
+        Console.WriteLine("Result: " + result);
+
+        Console.ReadKey();
+    }
+}
